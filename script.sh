7155341602773e2116abcdcf8b159ad98dc8f021
mmanoj2002 <mmanojkumarreddy2002@gmail.com>
2024-11-22 12:23:20 +0000

    ok File Installed

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#echo"setting up login page"
+#
+# Update Software
+# sudo apt update -y
+#
+# Install Utilities Software
+# sudo apt install zip unzip
+#
+# Install Nginx Web Server
+# sudo apt install -y nginx
+#
+# Clean Up Sample Files In Document
+# sudo rm -rf /var/www/html
+#
+# Create A New Document Root
+# sudo mkdir -p /var/www/html
+#
+# Deploy Login Page By Using ID
+# sudo git clone https://github.com/mmanoj2002/000123.git /var/www/html
